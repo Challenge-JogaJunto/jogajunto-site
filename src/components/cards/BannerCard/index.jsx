37aa81113f45d8eed7a5d6a5ff6e75d9303c0f7d
Thomaz <122468659+Th0mzzz@users.noreply.github.com>
2025-10-01 10:58:10 -0300

    feat: adicionando e corrigindo localização de arquivos, card e img do footer

--- src/components/cards/BannerCard/index.jsx
+++ src/components/cards/BannerCard/index.jsx
@@ -8,15 +8,15 @@ export default function ChampionshipBannerCard({championship}) {
     return (
         <>
             <ContainerDiv className={"flex flex-col-reverse lg:flex-row gap-10 px-8 py-4 text-[var(--texto)] relative"}>
-                <div className="flex gap-10 flex-col sm:flex-row">
-                    <div className="flex flex-col gap-5 items-center justify-center hidden lg:flex">
+                <div className="w-full flex gap-10 flex-col sm:flex-row ">
+                    <div className="w-full flex-col gap-5 items-center justify-center hidden xl:flex ">
                         <img src={championship.organizer.logo} alt="Logo do organizador"
-                             className={"min-w-[200px] max-w-[200px] aspect-ratio--1x1"}/>
+                             className={"min-w-[200px] max-w-[200px] aspect-ratio--1x1 rounded-full overflow-hidden"}/>
                         <p className="link">
                             {championship.organizer.name}
                         </p>
                     </div>
-                    <div className={"flex flex-col gap-3"}>
+                    <div className={"flex flex-col w-full gap-3"}>
                         <h3 className="subtitle mt-3">
                             {championship.championship.title}
                         </h3>
@@ -31,7 +31,7 @@ export default function ChampionshipBannerCard({championship}) {
                 </div>
 
                 <img src={championship.championship.coverImage} alt="imagem do campeonato"
-                     className={"w-full max-w-[500px] object-cover ml-auto aspect-video rounded-md"}/>
+                     className={"w-full object-cover lg:ml-auto rounded-md h-100 lg:h-62"}/>
             </ContainerDiv>
         </>
     )

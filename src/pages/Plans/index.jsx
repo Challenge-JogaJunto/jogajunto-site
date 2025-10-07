807aaae5d498c4a9a6343948a4fb83bd4311345e
Pedro Coutinho <pedro55496969@gmail.com>
2025-10-07 14:14:39 -0300

    feat: Pagina Planos e adição da pagina na rota

---
+++ src/pages/Plans/index.jsx
@@ -0,0 +1,14 @@
+import ContainerDiv from "@/components/Container/index.jsx";
+
+export default function Plans() {
+    return (
+        <ContainerDiv className={"flex flex-col align-items-center justify-center border-none"}>
+            <h3>Venha Jogar Junto com a gente!</h3>
+            <p>Assine um de nossos planos e obtenha oportunidades exclusivas! Mais visibilidade, mais ferramentas, mais organização, mais jogo!</p>
+            <div className={"border border-solid"}>
+                <p>TESTE</p>
+
+            </div>
+        </ContainerDiv>
+        )
+}

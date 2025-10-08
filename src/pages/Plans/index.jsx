719bde2ca6bb16d1ca380c8ee3eb264c36646c17
Pedro Coutinho <pedro55496969@gmail.com>
2025-10-08 13:39:24 -0300

    fix: Responsividade Página Planos

--- src/pages/Plans/index.jsx
+++ src/pages/Plans/index.jsx
@@ -7,7 +7,7 @@ import { GoCheckCircle } from "react-icons/go";
 
 export default function Plans() {
     return (
-        <div className={"flex flex-col align-items-center justify-center p-2 font-['Poppins'] "}>
+        <div className={"flex flex-col align-items-center justify-center p-2 font-['Poppins']"}>
             <h1 className={"font-semibold text-2xl m-1 font-variant[--ff-text]"}>Venha Jogar Junto com a gente!</h1>
             <p className={"m-1"}>Assine um de nossos planos e obtenha oportunidades exclusivas! Mais visibilidade, mais ferramentas, mais organização, mais jogo!</p>
             <div className={"border-2 border-solid border-[#6E3B96] rounded-md text-start p-6 text-[16px] font-light"}>
@@ -34,17 +34,18 @@ export default function Plans() {
                     <p className={"ml-2"}>Anúncios bloqueados</p>
                 </div>
             </div>
-            <div className={"flex align-items-center justify-center gap-48 mt-4 font-light "}>
-                <div className={"border-2 border-solid border-[#6E3B96] rounded-md w-[500px] p-3"}>
-                    <h1 className={"mb-2 text-[#6E3B96] font-semibold text-[25px]"}>Mensal</h1>
-                    <p className={"font-bold text-[#6E3B96] text-[12px] mt-4"}>Plano Anual</p>
-                    <p className={"text-[24px] font-bold mt-1 mb-1"}> R$ 15,00 / mês</p>
+
+            <div className={"flex align-items-center justify-center mt-4 font-light gap-5 w-[100%] flex-wrap lg:flex-nowrap "}>
+                <div className={"border-2 border-solid border-[#6E3B96] rounded-md w-[100%] p-3"}>
+                    <h1 className={"mb-2 text-[#6E3B96] font-semibold lg:text-[25px] text-[20px] "}>Mensal</h1>
+                    <p className={"font-bold text-[#6E3B96] lg:text-[16px] text-[14px] mt-4"}>Valor</p>
+                    <p className={"lg:text-[24px] text-[18px] font-bold mt-1 mb-1"}> R$ 30,00 / mês</p>
                     <Button styleClass={"w-[100%] mt-1"}>Assine Agora</Button>
                 </div>
-                <div className={"w-[500px] border-2 border-solid border-[#59C9A5] rounded-md p-3"}>
-                    <h1 className={"mb-2 text-[#59C9A5] font-semibold text-[25px]"}>Anual</h1>
-                    <p className={"font-bold text-[12px] text-[#59C9A5] mt-4"}>Plano Anual</p>
-                    <p className={"text-[24px] font-bold mt-1 mb-1"}>R$ 35,00/mês</p>
+                <div className={"w-[100%] border-2 border-solid border-[#59C9A5] rounded-md p-3"}>
+                    <h1 className={"mb-2 text-[#59C9A5] font-semibold lg:text-[25px] text-[20px]"}>Anual</h1>
+                    <p className={"font-bold lg:text-[16px] text-[14px] text-[#59C9A5] mt-4"}>Valor</p>
+                    <p className={"lg:text-[24px] text-[18px] font-bold mt-1 mb-1"}>R$ 220,00 / ano</p>
                     <Button variant={"secondary"} styleClass={"w-[100%] mt-1"} >Assine Agora</Button>
                 </div>
             </div>
